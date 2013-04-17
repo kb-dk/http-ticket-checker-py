0fc82b885f8837187650390f1221a4317f8bd98b
Adam Tulinius <aft@statsbiblioteket.dk>
2013-04-17 10:20:07 +0200

    rename variables as part of review followup, and obey userIdentifier

--- http_ticket_checker.py
+++ http_ticket_checker.py
@@ -44,11 +44,10 @@ def get_file(resource):
     resource_id = resource.split("/")[-1].split(".")[0]
     print resource_id
 
-
     valid_ticket = False
 
     if ticket_id and ticket_id is not "":
-        valid_ticket = validate_ticket(ticket_id, resource_id)
+        valid_ticket = validate_ticket(ticket_id, resource_id, request.remote_addr)
 
     if valid_ticket:
         return send_from_directory(app.config['FILE_DIR'],
@@ -102,26 +101,26 @@ def http_backend_factory(resolve_ticket_url):
     return http_backend
 
 
-def validate_ticket(ticket_id, resource):
+# String -> String -> Boolean
+def validate_ticket(ticket_id, resource_id, user_identifier):
     if ticket_id_pattern.match(ticket_id):
-        ticket = get_ticket(ticket_id)
-        ticket = parse_ticket(ticket)
-        if ticket:
-            resources, type_, user_id = ticket
+        raw_ticket = get_ticket(ticket_id)
+        parsed_ticket = parse_ticket(raw_ticket)
+        if parsed_ticket:
+            resource_ids, presentation_type, user_id = parsed_ticket
             # strip everything up to, and including, "..uuid:" from the resource-id's
-            resources = map(lambda r: r.split(":")[-1], resources)
-            valid = (app.config['CONTENT_TYPE'] == type_ and resource in resources)
-            if valid:
-                return True
-            else:
-                return False
-            
+            stripped_resource_ids = map(lambda r: r.split(":")[-1], resource_ids)
+            is_valid = (app.config['CONTENT_TYPE'] == presentation_type
+                and resource_id in stripped_resource_ids
+                and user_identifier == user_id)
+            return is_valid
+
     return False
 
-
+# Ticket -> (String, String, String)
 def parse_ticket(ticket):
     if ticket:
-        if not False in [ticket.has_key(key) for key in ["resources", "type", "userIdentifier"]]:
+        if not False in [key in ticket for key in ["resources", "type", "userIdentifier"]]:
             return ticket["resources"], ticket["type"], ticket["userIdentifier"]
         else:
             return None
